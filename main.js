fd94983b8344b49742f732e324b40934e536114f
G-ctrl600 <gowric2003@gmail.com>
2025-09-03 18:14:16 +0530

    feat:app structure

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import express from 'express';
 let app=express();
 let PORT=4000;
 app.get('/',(req,res)=>{
-    res.json("hello world")
+    res.json({msg : "hello students!"})
 })
 app.listen(PORT,()=>{
     console.log(`server is running at port  http://localhost:${PORT}`);
