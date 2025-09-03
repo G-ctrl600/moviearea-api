8a413d97ee05d7dc55debac08191f4fa75818917
G-ctrl600 <gowric2003@gmail.com>
2025-09-03 18:27:28 +0530

    route structure

--- main.js
+++ main.js
@@ -5,6 +5,29 @@ let PORT=4000;
 app.get('/',(req,res)=>{
     res.json({msg : "hello students!"})
 })
+
+//CURD functionality of movies
+// R- for Read
+
+app.get('/movies',()=>{
+
+})
+
+//c- for create
+app.get('/movies',()=>{
+
+})
+
+//u- for update
+app.put('/movies/:id',()=>{
+
+})
+
+// D - for delete
+app.delete('/movies/:id',()=>{
+
+})
+
 app.listen(PORT,()=>{
     console.log(`server is running at port  http://localhost:${PORT}`);
 })
